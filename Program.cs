1d85f883bb27e6443761f35d3ca795ffce13fd06
ALEX K. KEMBOI <AKEMBOI@fintech-group.com>
2023-06-20 16:53:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Progran
+{
+    static void Main(string[] args)
+    {
+        string[] dictionary = { "apple", "orange", "mango", "banana", "ovacado" };
+        Array.Sort(dictionary);
+        foreach (string key in dictionary)
+        {
+            Console.WriteLine(key);
+        }
+    }
+}
